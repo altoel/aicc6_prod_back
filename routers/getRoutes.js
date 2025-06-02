2d55eb1c832dd2b3b84d8cf65e29d993db7f9575
altoe <alto.ellya@gmail.com>
2025-06-02 10:29:33 +0900

    get route get_task -> get_tasks

--- routers/getRoutes.js
+++ routers/getRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { getTasks } = require('../controllers/getTasks');
 
 // :userId는 정해지지 않은 문자열이며 params로 받는다.
-router.get('/get_task/:userId', getTasks);
+router.get('/get_tasks/:userId', getTasks);
 
 module.exports = router;
